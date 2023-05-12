17717df4afbe813018169c824de87c5803140304
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-12 12:48:43 +0200

    completed second exercise

---
+++ src/org/java/myset/MainSet.java
@@ -0,0 +1,19 @@
+package org.java.myset;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class MainSet {
+	public static void main(String[] args){
+		
+		Random random = new Random();
+		Set<Integer> randomNumbers = new HashSet<>();
+
+		while(randomNumbers.size() != 5) {
+			randomNumbers.add(random.nextInt(2,12));
+		}
+
+		System.out.println(randomNumbers);
+	}
+}
