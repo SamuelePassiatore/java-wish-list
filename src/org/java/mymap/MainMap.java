1fa7036c6065fdd87764f4e01a7f8ab77ecd4b89
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-12 14:56:32 +0200

    completed third exercise

---
+++ src/org/java/mymap/MainMap.java
@@ -0,0 +1,26 @@
+package org.java.mymap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMap {
+	public static void main(String[] args){
+		
+		Map<String, Integer> wordMap = new HashMap<>();
+		String str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam - repudiandae est, alias in ullam excepturi ipsam modi odio Lorem ipsum dolor asperiores voluptatum, # quam distinctio ! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repudiandae Totam repudiandae est ; accusantium voluptatum, quam distinctio aut magnam quod veniam esse nesciunt debitis.";
+		String[] words = str.split(" ");
+		
+		for (String word : words) {
+			      if (word.length() == 0) {
+			        continue;
+			      }
+			      if (!wordMap.containsKey(word)) {
+			        wordMap.put(word, 1);
+			      } else {
+			        wordMap.put(word, wordMap.get(word) + 1);
+			      }
+		}
+
+		System.out.println(wordMap);
+	}
+}
