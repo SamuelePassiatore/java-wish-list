73a99765be46d73f2448a5034ccecb59f16a9025
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-12 12:08:10 +0200

    completed exercise

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -6,6 +6,30 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
+		List<String> wishList = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		boolean addNewWish = true;
 		
+		while(addNewWish) {
+			System.out.println("Inserisci un desiderio: ");
+			String wish = sc.nextLine();
+			wishList.add(wish);
+			
+			System.out.println("Elementi nella lista dei desideri: " + wishList.size());
+			System.out.println("Vuoi inserire un altro desiderio? (s/n) : ");
+			String response = sc.nextLine();
+			
+			if(response.equals("n")) {
+				addNewWish = false;
+			}
+		}
+		
+		System.out.println("Ecco la lista dei tuoi desideri:");
+		
+		for (String wish : wishList) {
+			
+			System.out.println("- " + wish);
+		}
+		sc.close();
 	}
 }
